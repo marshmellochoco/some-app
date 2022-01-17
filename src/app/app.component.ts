7f114eadf847f6dbb62eabc8ab1c55503686c32d
Ng Jing Kai <37670295+marshmellochoco@users.noreply.github.com>
2022-01-17 16:03:35 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'some-app';
+  items = [{
+    src: "assets/img1.jpg",
+    alt: "space guy 1",
+    text: "This is just some image that I got from the internet. Nothing too special. 🎈",
+  }, {
+    src: "assets/img2.jpg",
+    alt: "space guy 2",
+    text: "This is just some image that I got from the internet. Nothing too special. ☕",
+
+  }, {
+    src: "assets/img3.jpg",
+    alt: "space guy 3",
+    text: "This is just some image that I got from the internet. Nothing too special. 🚀",
+  },]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeItemComponent } from './home-item/home-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeItemComponent,
   ],
   imports: [
     BrowserModule

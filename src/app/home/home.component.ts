1a15db13cfb231b04b8d733c61b2840ddb026357
Ng Jing Kai <jingkai.ng@fuel.com.my>
2022-01-19 12:00:27 +0800

    mock data for loadData

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,8 +16,25 @@ export class HomeComponent implements OnInit {
   }
 
   loadData() {
-    return this.productApi.getProducts().subscribe((data: {}) => {
-      this.items = data
-    })
+    this.items = [
+      {
+        src: "assets/img1.jpg",
+        alt: "space guy 1",
+        text: "This is just some image that I got from the internet. Nothing too special. 🎈",
+      },
+      {
+        src: "assets/img2.jpg",
+        alt: "space guy 2",
+        text: "This is just some image that I got from the internet. Nothing too special. ☕",
+      },
+      {
+        src: "assets/img3.jpg",
+        alt: "space guy 3",
+        text: "This is just some image that I got from the internet. Nothing too special. 🚀",
+      },
+    ];
+    // return this.productApi.getProducts().subscribe((data: {}) => {
+    //   this.items = data
+    // })
   }
 }
